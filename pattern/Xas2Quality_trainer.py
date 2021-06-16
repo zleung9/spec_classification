3ffb8ca0440f9f29a8fb90733e125591a14e3741
Zhu <zhul9311@gmail.com>
2021-06-16 12:47:07 -0400

    implement dataloader in train()

--- pattern/Xas2Quality_trainer.py
+++ pattern/Xas2Quality_trainer.py
@@ -11,8 +11,8 @@ import time
 from torch.utils.tensorboard import SummaryWriter
 
 class Trainer:
-    def __init__(self, model, data,
-                 device = torch.device('cuda'), verbose=True, board=False,
+    def __init__(self, model, train_loader, test_loader, val_loader, loss_constrain_test_loader=None, 
+                 device = torch.device('cuda'), verbose=True, ts_board=False,
                  learning_rate=1e-4, max_epochs=1000, batch_size=100):
         
         # configuration paremeters
@@ -20,85 +20,99 @@ class Trainer:
         self.batch_size = batch_size
         self.max_epochs = max_epochs
         self.verbose = verbose
-        self.tensorboard = board
+        self.tensorboard = ts_board
         self.learning_rate = learning_rate
         
         # process input data and label data (will be replaced by dataloader)
-        self.x_train, self.y_train = data['train']
-        self.x_val, self.y_val = data['val']
-        self.N_train = self.x_train.size()[0]
-        self.N_val = self.x_val.size()[0]
-        
+
+        self.train_loader = train_loader
+        self.val_loader = val_loader
+        self.test_loader = test_loader
+        # this dataloader is used for pulling the extreme probability: 0->0.3, 1->0.7, see "def train(self)"
+        self.loss_constrain_loader = loss_constrain_test_loader
+
         # define model, loss function and optimizer
         self.model = model.to(self.device)
         self.loss_BCE = nn.BCELoss(reduction='mean').to(self.device)
         self.solver: optim.Optimizer = optim.RMSprop(model.parameters(), lr=self.learning_rate)
         # zero_grad
 
-    def train(self, test_set):
-        
-        test_norm, test_label = test_set
+    def train(self):
 
         if self.tensorboard:
             writer = SummaryWriter(comment="_train_all-bvs")
             print("Tensorboard is on")
         else:
-            werite = None
+            writer = None
             print("Tensorboard is off")
                 
-        train_loss = np.zeros(self.max_epochs)
-        val_loss = np.zeros(self.max_epochs)
+        self.train_loss = np.zeros(self.max_epochs)
+        self.val_loss = np.zeros(self.max_epochs)
 
         if self.verbose: start = time.time() # start time
         for epoch in range(self.max_epochs):
             self.model.train() # set to training mode
 
-            permutation = torch.randperm((self.N_train),device=self.device) # random permutation of data
-            permutation_test = torch.randperm((test_norm.shape[0]),device=self.device) # 0611
-            for i in range(0, self.N_train, self.batch_size):     
-                self.model.zero_grad() # clear up the gradients in the model
-                indices = permutation[i:i+self.batch_size] # minibatch
-                y_pred = self.model(self.x_train[indices]).to(device=self.device) # predict values
-                loss = self.loss_BCE(y_pred, self.y_train[indices]) # calculate loss
-                loss.backward() # compute the gradients of loss in the graph
-                self.solver.step() # update weights and paremeters
-                
-                self.model.zero_grad()
-                indices_test = permutation_test[i:i+self.batch_size] # 0611
-                y_pred = self.model(test_norm[indices_test]).to(device=self.device) # predict values #0611
-                perc30, perc70 = torch.quantile(y_pred, tensor([0.3, 0.7]).to(device=self.device))
-                
-                i_invalid30 = (y_pred > perc30) & (y_pred < 0.3)
-                invalid_pred30 = y_pred[i_invalid30]
-                loss30 = self.loss_BCE(invalid_pred30, torch.full_like(invalid_pred30, fill_value=0.3))
-                
-                i_invalid70 = (y_pred < perc70) & (y_pred > 0.7)
-                invalid_pred70 = y_pred[i_invalid70]
-                loss70 = self.loss_BCE(invalid_pred70, torch.full_like(invalid_pred70, fill_value=0.3))
-                
-                loss_center = loss30 + loss70
-                loss_center = loss_center * 0.5 # avoid distorting the good result
-                loss_center.backward()
-                self.solver.step() # update weights and paremeters
 
-            self.model.eval() # set to evaluation mode
-            train_loss[epoch] = self.loss_BCE(self.model(self.x_train),self.y_train) # average loss
-            val_loss[epoch] = self.loss_BCE(self.model(self.x_val),self.y_val)
-           
+            loss_train_list = []
+            #train_loader can only be exhausted if its length < that of loss_constrain_loader
+            for trn, tst in zip(self.train_loader, self.loss_constrain_loader):
+                spec_in, label_in = trn
+                spec_in = spec_in.to(self.device)
+                label_in = label_in.to(self.device)
+
+                self.model.zero_grad()
+                label_pred = self.model(spec_in)
+                loss = self.loss_BCE(label_pred, label_in)
+                loss_train_list.append(loss) # list of training loss for the current epoch
+                loss.backward()
+                self.solver.step()
+
+                # this part tries to minimize loss w.r.t. 0.3 & 0.7 so predictions are pulled to center
+                spec_tst, label_tst = tst
+                spec_tst = spec_tst.to(self.device)
+                label_tst = label_tst.to(self.device)
+                self.model.zero_grad()
+                y_pred = self.model(spec_tst).to(device=self.device)
+                # only predictions that fall in [perc30, perc70] (not-so-sure predictions) are affected.
+                perc30, perc70 = torch.quantile(y_pred, tensor([0.3,0.7]).to(device=self.device))
+                invalid_pred30 = y_pred[(y_pred>perc30) & (y_pred<0.3)]
+                loss_perc30 = self.loss_BCE(invalid_pred30, torch.full_like(invalid_pred30, fill_value=0.30))
+                invalid_pred70 = y_pred[(y_red<perc70) & (y_pred>0.7)]
+                loss_perc70 = self.loss_BCE(invalid_pred70, torch.full_like(invalid_pred70, full_value=0.70))
+                loss_center = (loss_perc30 + loss_perc70) * 0.5 # avoid distorting the good result too much
+                loss_center.backward()
+                self.solver.step()
+
+            # caculate the loss for validition set.
+            self.model.eval()
+            loss_val_list = []
+            for spec_in, label_in in self.val_loader:
+                spec_in = spec_in.to(self.device)
+                label_in = label_in.to(self.device)
+                label_pred = self.model(spec_in)
+                loss = self.loss_BCE(label_pred, label_in)
+                loss_val_list.append(loss) # list of validation loss for the current epoch
+                
+            self.train_loss[epoch] = np.mean(loss_train_list)
+            self.val_loss[epoch] = np.mean(loss_val_list)
+
             try:
-                writer.add_scalar("Loss/train", train_loss[epoch], epoch) 
-                writer.add_scalar("Loss/val", val_loss[epoch], epoch)
+                writer.add_scalar("Loss/train", self.train_loss[epoch], epoch) 
+                writer.add_scalar("Loss/val", self.val_loss[epoch], epoch)
             except:
                 pass
             if self.verbose and epoch%100==99:
                 print('Epoch {:d}/{:d} | loss: {:.3f}/{:.3f}'
                       .format(epoch+1, self.max_epochs, train_loss[epoch],val_loss[epoch]))    
         try: 
-            writer.flush()
+            writer.flush() # flush the writer opject
         except:
             pass
+        
         if self.verbose:
             print("Total time: {:.1f} min.".format((time.time()-start)/60))  
-        
-        return train_loss, val_loss
 
+
+    def test(self):
+        pass
