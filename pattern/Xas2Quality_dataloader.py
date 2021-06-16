c107675270090e5100b88837a9d906bb09008c8f
Zhu <zhul9311@gmail.com>
2021-06-16 12:36:04 -0400

    add dataloader feature, remove self.balance

--- pattern/Xas2Quality_dataloader.py
+++ pattern/Xas2Quality_dataloader.py
@@ -1,44 +1,49 @@
-import torch
-from torch import nn, tensor, optim
-from torch.utils.data import Dataset, DataLoader, dataset
-
-import os, glob, pickle
+import pickle
+import math
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-import time
+
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torch.utils.data.dataset import Subset
+from torch.utils.data.sampler import WeightedRandomSampler
+
 
 from torch.utils.tensorboard import SummaryWriter
 class Xas2QualityDataset(Dataset):
-    def __init__(self, spec, label, feature, energy_cut, verbose=False, balance=False):
+    def __init__(self, spec, label, feature, energy_cut, verbose=False):
         super(Xas2QualityDataset, self).__init__()
         
         self.verbose = verbose
-        self.spec = spec[:, (feature>energy_cut[0])&(feature<energy_cut[1])]
-        self.spec_label = label
-        self.feature = feature[(feature>energy_cut[0])&(feature<energy_cut[1])]
-        self.feature_size = self.feature.shape[0]
-        self.output_size = self.spec_label.shape[0]
-        self.balanced = balance
 
-        # only select label<0.000001 or label>0.000009 as valid data
-        select_label = (self.spec_label>1-1e-6) | (self.spec_label<0+1e-6)
+        # select a certain range of features for training
+        feature_range = (feature>energy_cut[0])&(feature<energy_cut[1])
+        self.feature = feature[feature_range]
+        self.feature_size = self.feature.shape[0]
+
+        # only select label<0.001 or label>0.009 as valid data
+        self.spec = spec[:, feature_range]
+        self.spec_label = label
+        select_label = (self.spec_label>=0) & ((1-1e-3<self.spec_label)|(self.spec_label<0+1e-3))
         self.data = self.spec[select_label]
         self.label = self.spec_label[select_label]
-        print("Orignal | Label_1: {:d}, Label_0: {:d}".format((self.label>1-1e-6).sum(),(self.label<1e-6).sum()))
+        self.sample_size = self.label.shape[0]
 
-        # balance and shuffle data so there are equaly amount of 0 and 1.
-        if balance: 
-            self.balance()
-            self.balanced = True
-        print("Feature size: {}".format(self.feature_size))
-        assert self.data.shape[0] == self.label.shape[0]
+        if verbose:
+            print("Orignal Label\n1: {:d}, 0: {:d}, -1: {:d}"
+                  .format((self.label>0.5).sum(),(self.label<0.5).sum(),(self.spec_label==-1).sum()))
+        
+        # create a weight sequence of good and  d data according to their fractions
+        good_mask, bad_mask = self.label>0.5, self.label<0.5
+        self.weights = good_mask * bad_mask.sum() + bad_mask * good_mask.sum()
+        
+        # data is not partitioned into train, test, validation sets by default
+        self.has_partition = False
 
 
     def __len__(self):
         return self.data.shape[0]
     
-    
+
     def __getitem__(self,idx):
         if torch.is_tensor(idx):
             idx = idx.tolist()
@@ -46,43 +51,27 @@ class Xas2QualityDataset(Dataset):
         return sample
 
     
-    def balance(self):
-        "Sample equal amount of good and bad data from original spec, and shuffle them"
-        label_1 = self.spec_label > 1-1e-6
-        label_0 = self.spec_label < 0+1e-6
-        N_label_1 = label_1.sum()
-        N_label_0 = label_0.sum()
-        
-        if N_label_1 > N_label_0: # if more good data than bad data, sample euqal amount of good data
-            one_zero_match = np.random.randint(N_label_1,size=N_label_0) 
-            data = np.vstack((self.spec[label_1][one_zero_match], self.spec[label_0]))
-            
-        else: # if less good data than bad data, sample euqal amount of bad data
-            one_zero_match = np.random.randint(N_label_0,size=N_label_1)
-            data = np.vstack((self.spec[label_1],self.spec[label_0][one_zero_match]))
+    def partition(self, ratio=(0.7, 0.15, 0.15)):
+        '''
+        Partition the dataset into train, validation and test set according to ratio.
+        '''
+        N_samples = self.sample_size
 
-        # label 1 for first half of data, label 0 for second half
-        label = np.zeros((data.shape[0], 1))
-        label[:int(data.shape[0]/2)] = 1 # label the first half as 1
-        
-        # randomly shuffle data and labels
-        shuffle = np.random.permutation(data.shape[0])
-        self.data = data[shuffle]
-        self.label = label[shuffle]
-        
-        self.feature_size = self.data.shape[1]
-        self.output_size = self.label.shape[1]
-            
-        if self.verbose:
-            print("Balanced | Label_1: {}; Label_0: {}".format(self.label.sum(),(1-self.label).sum()))
+        N_train = int(np.floor(N_samples * ratio[0]))
+        N_test = int(np.floor(N_samples * ratio[1]))
+        N_val = N_samples - N_train - N_test
 
-    def create_train_val_test(self):
-        pass
+        index_shuffle = np.random.permutation(range(N_samples)) # shuffle data and label
+        self.index_train = index_shuffle[:N_train]
+        self.index_test = index_shuffle[N_train: N_train+N_test]
+        self.index_val = index_shuffle[-N_val:]
+
+        self.has_partition = True
     
-    
+
     @classmethod
     def from_file(cls, spec_file, quality_file, energy_cut=(7600,8000), splits=[0.7,0.2,0.1], 
-                  verbose=True, balance=False):
+                  verbose=True):
 
         # load normalized spec data from file
         with open(spec_file,'rb') as f:
@@ -94,23 +83,43 @@ class Xas2QualityDataset(Dataset):
         with open(quality_file,'rb') as f:
             result = pickle.load(f)
         labels = result['label']
-        sets = result['set']
+        sets = np.array(result['set'])
+        labels[sets=='test'] = -1 # unlabeled data has label -1
 
-        # get rid of labels that fall in between [0.1, 0.9]
-        label_select = (labels>=0.9) | (labels<=0.1)
+        # # get rid of labels that fall in between [0.1, 0.9]
+        # label_select = (labels>=0.9) | (labels<=0.1)
 
-        return Xas2QualityDataset(spec=red_norm[label_select], label=labels[label_select], feature=feature_grid, 
-                                  energy_cut=energy_cut, verbose=verbose, balance=balance)
+        return Xas2QualityDataset(spec=red_norm, label=labels, feature=feature_grid,
+                                  energy_cut=energy_cut, verbose=verbose)
 
    
-# def get_Xas2Quality_dataloaders():
 
-#     for name in ['train', 'test', 'set']:
+def get_Xas2Quality_dataloaders(dataset, batch_size, ratio=(0.7, 0.15, 0.15), load_unlabel=False):
     
+    # partition the totald dataset into train,validation and test sets according to ratio
+    dataset.partition(ratio=ratio)
+    if not dataset.has_partition: # has_partion attribute is only turned on after partition
+        print("Data partition failed.")
+        return None, None, None
+    else:
+        ds_train = Subset(dataset, dataset.index_train)
+        ds_test = Subset(dataset, dataset.index_test)
+        ds_val = Subset(dataset, dataset.index_val)
 
-#     train_loader = Dataloader(ds_train, batch_size=batch_size, shuffle=True)
-#     val_loader = Dataloader(ds_val, batch_size=batch_size, shuffle=True)    
-#     test_loader = Dataloader(ds_test, batch_size=batch_size, shuffle=True)
+    train_sampler = WeightedRandomSampler(dataset.weights[dataset.index_train], replacement=True,
+                                          num_samples=math.ceil(len(ds_train)/batch_size)*batch_size)
 
-#     return train_loader, val_loader, test_loader
-        
+    train_loader = DataLoader(ds_train, batch_size=batch_size, num_workers=0, pin_memory=False,
+                              sampler=train_sampler)
+    val_loader = DataLoader(ds_val, batch_size=batch_size, num_workers=0, pin_memory=False)
+    test_loader = DataLoader(ds_test, batch_size=batch_size, num_workers=0, pin_memory=False)
+
+    # create a dataloader for unlabeled data for extreme prob. reduction (see Trainer.train())
+    if load_unlabel: 
+        index_all = np.arange(len(dataset.spec))
+        index_unlabeled = index_all[dataset.spec_label==-1]
+        ds_unlabeled = Subset(dataset, index_unlabeled)
+        unlabel_loader = DataLoader(ds_unlabeled, batch_size=batch_size, num_workers=0, pin_memory=False)
+        return train_loader, val_loader, test_loader, unlabel_loader
+
+    return train_loader, val_loader, test_loader 
